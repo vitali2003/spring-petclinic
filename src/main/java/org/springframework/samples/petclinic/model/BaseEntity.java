e03e8370ca798f4115da00307dd43bd951ebfba4
Vitali Bazhko <vitaly2003@gmail.com>
2025-05-29 18:16:05 +0300

    Made more changes

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -36,14 +36,12 @@ public class BaseEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	public boolean isNew() {
+	public boolean isNew() {
 	public boolean isNew() {
 		return this.id == null;
 	}
